3010282af4289625ed9738fca0c300a38866ea64
30isnottoolate <akos.varga.92@gmail.com>
2022-07-15 18:32:31 +0200

    Add basic elements to app

--- src/script.js
+++ src/script.js
@@ -6,7 +6,27 @@ class Calculator extends React.Component {
   render() {
     return (
       <div id="app">
-        <p>+-*/</p>
+        <div id="formula">123</div>
+        <div id="display">123</div>
+        <div id="buttons">
+          <button id="zero">0</button>
+          <button id="one">1</button>
+          <button id="two">2</button>
+          <button id="three">3</button>
+          <button id="four">4</button>
+          <button id="five">5</button>
+          <button id="six">6</button>
+          <button id="seven">7</button>
+          <button id="eight">8</button>
+          <button id="nine">9</button>
+          <button id="decimal">.</button>
+          <button id="add">+</button>
+          <button id="subtract">-</button>
+          <button id="multiply">*</button>
+          <button id="divide">/</button>
+          <button id="clear">C</button>
+          <button id="equals">=</button>
+        </div>
       </div>
     );
   }
