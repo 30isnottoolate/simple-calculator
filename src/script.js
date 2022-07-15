05332f05b5d8a652b21da013dc052053a8ccceec
30isnottoolate <akos.varga.92@gmail.com>
2022-07-15 18:44:17 +0200

    Add classNames to elements

--- src/script.js
+++ src/script.js
@@ -6,26 +6,26 @@ class Calculator extends React.Component {
   render() {
     return (
       <div id="app">
-        <div id="formula">123</div>
-        <div id="display">123</div>
+        <div id="formula" className="displays">123</div>
+        <div id="display" className="displays">123</div>
         <div id="buttons">
-          <button id="zero">0</button>
-          <button id="one">1</button>
-          <button id="two">2</button>
-          <button id="three">3</button>
-          <button id="four">4</button>
-          <button id="five">5</button>
-          <button id="six">6</button>
-          <button id="seven">7</button>
-          <button id="eight">8</button>
-          <button id="nine">9</button>
-          <button id="decimal">.</button>
-          <button id="add">+</button>
-          <button id="subtract">-</button>
-          <button id="multiply">*</button>
-          <button id="divide">/</button>
-          <button id="clear">C</button>
-          <button id="equals">=</button>
+          <button id="zero" className="blues">0</button>
+          <button id="one" className="blues">1</button>
+          <button id="two" className="blues">2</button>
+          <button id="three" className="blues">3</button>
+          <button id="four" className="blues">4</button>
+          <button id="five" className="blues">5</button>
+          <button id="six" className="blues">6</button>
+          <button id="seven" className="blues">7</button>
+          <button id="eight" className="blues">8</button>
+          <button id="nine" className="blues">9</button>
+          <button id="decimal" className="blues">.</button>
+          <button id="add" className="oranges big-butt">+</button>
+          <button id="subtract" className="oranges">-</button>
+          <button id="multiply" className="oranges">*</button>
+          <button id="divide" className="oranges">/</button>
+          <button id="clear" className="oranges">C</button>
+          <button id="equals" className="oranges big-butt">=</button>
         </div>
       </div>
     );
