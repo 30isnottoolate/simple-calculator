58d2c5ccec7e9b0f7f6a056e169f9d49ec3125c4
30isnottoolate <107811595+30isnottoolate@users.noreply.github.com>
2022-07-25 17:00:08 +0200

    Render Calculator from index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import './script.js';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Calculator from './Calculator';
+
+const container = ReactDOM.createRoot(document.getElementById('app-container'));
+container.render(
+  <React.StrictMode>
+    <Calculator />
+  </React.StrictMode>
+);
